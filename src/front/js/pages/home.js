d9e916ee842a2a47cb128b8f3f4706a90852cba4
Dans182 <dgaiteiro@gmail.com>
2022-07-13 23:18:01 +0000

    v2

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,30 +3,56 @@ import { Context } from "../store/appContext";
 /* import "../../styles/home.css"; */
 
 export const Home = () => {
-    const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-    return (
-        <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img src="https://s1.1zoom.me/big3/655/356483-terminator22.jpg" className="d-block w-100" alt="travel" />
-                </div>
-                <div className="carousel-item">
-                    <img src="https://s1.1zoom.me/b4957/156/Rome_Italy_Ruins_Arch_Septimius_Severus_Temple_532568_1920x1080.jpg" className="d-block w-100" alt="Roma" />
-                </div>
-                <div className="carousel-item">
-                    <img src="https://www.wallpapertip.com/wmimgs/8-87339_541446-title-man-made-santorini-towns-greece-sea.jpg" className="d-block w-100" alt="
-Grecia" />
-                </div>
-            </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
+  return (
+    <div
+      id="carouselExampleControls"
+      className="carousel slide"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img
+            src="https://s1.1zoom.me/big3/655/356483-terminator22.jpg"
+            className="d-block w-100"
+            alt="travel"
+          />
         </div>
-    );
+        <div className="carousel-item">
+          <img
+            src="https://s1.1zoom.me/b4957/156/Rome_Italy_Ruins_Arch_Septimius_Severus_Temple_532568_1920x1080.jpg"
+            className="d-block w-100"
+            alt="Roma"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://www.wallpapertip.com/wmimgs/8-87339_541446-title-man-made-santorini-towns-greece-sea.jpg"
+            className="d-block w-100"
+            alt="
+Grecia"
+          />
+        </div>
+      </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleControls"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleControls"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,28 +1,33 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			logged: null
-		},
-		actions: {
-			verify: async () => {
-				try {
-					const resp = await fetch("https://3001-4geeksacade-reactflaskh-1n3r28z34jr.ws-eu44.gitpod.io/api/protected", {
-						method: "GET",
-						headers: { "Content-Type": "application/json", 'Authorization': 'Bearer ' + localStorage.getItem("token") },
-					});
-					const data = await resp.json();
-					setStore({ logged: data.logged_in || false });
-				} catch (e) {
-					setStore({ logged: false });
-				};
-			},
-			logout: async () => {
-				localStorage.clear();
-				setStore({ logged: false });
-			}
-
-		}
-	};
+  return {
+    store: {
+      logged: null,
+    },
+    actions: {
+      verify: async () => {
+        try {
+          const resp = await fetch(
+            "https://3001-georgelion-jwtauthentic-s5y82hkrjck.ws-eu53.gitpod.io/api/protected",
+            {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + localStorage.getItem("token"),
+              },
+            }
+          );
+          const data = await resp.json();
+          setStore({ logged: data.logged_in || false });
+        } catch (e) {
+          setStore({ logged: false });
+        }
+      },
+      logout: async () => {
+        localStorage.clear();
+        setStore({ logged: false });
+      },
+    },
+  };
 };
 
 export default getState;
